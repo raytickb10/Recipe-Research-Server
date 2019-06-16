ce6992c341f93e292a3babd39199f3e7304ea25f
raytickb10 <raytickb10@students.ecu.edu>
2019-06-16 04:20:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ let server;
 function runServer(databaseUrl, port = PORT) {
 
   return new Promise((resolve, reject) => {
+    mongoose.set('useCreateIndex', true);
     mongoose.connect(databaseUrl, {useNewUrlParser: true} , err => {
       if (err) {
         return reject(err);
