0a0c0e441b8c668e5c5eaaf7b30ae9cc036ddeca
raytickb10 <raytickb10@students.ecu.edu>
2019-06-16 19:56:00 -0400

    Update router.js

--- auth/router.js
+++ auth/router.js
@@ -26,7 +26,7 @@ router.post('/login', localAuth, (req, res) => {
 const jwtAuth = passport.authenticate('jwt', {session: false});
 
 // The user exchanges a valid JWT for a new one with a later expiration
-router.post('/auth/refresh', jwtAuth, (req, res) => {
+router.post('/refresh', jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
   res.json({authToken});
 });
